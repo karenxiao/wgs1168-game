52815a91f78755e264377042dd1b0753c97b3f50
Karen Xiao <karenxiao240@gmail.com>
2013-04-19 22:50:28 -0400

    functional

--- game.js
+++ game.js
@@ -4,49 +4,72 @@ var images = [
 	["3a.jpg", "3b.jpg", "b", "Linda Cureton, NASA, CIO", "Linda Cureton has the coolest job. She's the CIO for NASA. And NASA has all the coolest technology in the world from the fastest computer networks to a bunch of stuff used to conduct experiments in outer space."],
 	["4a.jpg", "4b.jpg", "b", "Jack Dorsey, CEO, Twitter, Square", "Jack Dorsey is an American web developer and businessman widely known as the creator of Twitter and as the founder and CEO of Square, a mobile payments company"],
 	["5a.jpg", "5b.jpg", "a", "Grace Murray Hopper", "A pioneer in the field, Grace Murray Hopper was one of the first programmers of the Harvard Mark I computer, and developed the first compiler for a computer programming language. She conceptualized the idea of machine-independent programming languages, which led to the development of COBOL, one of the first modern programming languages"],
-	["6a.jpg", "6b.jpg", "b", "Kimber Lockhart, director of engineering, Box", "At Box, Kimber Lockhart leads the web application engineering team that builds most new features on Box. For instance, Lockhart was responsible for a major redesign and rebuild of the Box user interface."],
-	["7a.jpg", "7b.jpg", "a", "David Malan, Instructor, CS50", "David Malan is the instructor for Computer Science 50, Harvard’s introductory Computer Science course. He is on a mission to catapult computer science to the forefront of students’ academic interests and reinvent the way computer science is taught and viewed by students."],
-
+	["6a.jpg", "6b.jpg", "b", "Kimber Lockhart, director of engineering, Box", "At Box, Kimber Lockhart leads the web application engineering team that builds most new features on Box. For instance, Lockhart was responsible for a major redesign and rebuild of the Box user interface."]
 ]
 
 var a = document.getElementById("image-a");
 var b = document.getElementById("image-b");
 var result = document.getElementById("result");
+var end = document.getElementById("end").style.visibility = "hidden";
 var num = 0;
 
-function start()
+function startGame()
 {
 
 	// remove start button
-	document.getElementById("start").style.visibility = "hidden";
+	document.getElementById("start").style.display = "none";
+	end.visibility = "hidden";
 
 	// load initial images
-	loadImages();
-
-}
-
-function loadImages()
-{
 	a.src = "images/" + images[num][0];
 	b.src = "images/" + images[num][1];
+
 }
 
 function imageSelected(image)
 {
 	if (image == images[num][2])
 	{
-		result.innerHTML = "Correct!<hr><b>" + images[num][3] + "</b><br>" + images[num][4] + "<hr><a onclick='next()'>Next</a>"
-;
+		result.style.visibility = "visible";
+		result.innerHTML = "Correct!<hr><b>" + images[num][3] + "</b><br>" + images[num][4] + "<hr><a onclick='next()'>Next</a>";
 	}
 	else
 	{
-		result.innerHTML = "Incorrect!<br>" + images[num][3] + "<br><a onclick='next()'>Next</a>"
+		result.style.visibility = "visible";
+		result.innerHTML = "Incorrect! Try again.<hr><a onclick='next()'>Next</a>";
 	}
+}
+
+function loadImages()
+{
+	a.src = "images/" + images[num][0];
+	a.onclick = imageSelected('a');
+	b.src = "images/" + images[num][1];
+	b.onclick = imageSelected('b');
+}
+
+function endGame()
+{
+	a.style.display = "none";
+	b.style.display = "none";
+	result.style.display = "none";
+	end.style.visibility = "visible";
 }
 
 function next()
 {
-	result.style.visibility = "hidden";
-	num += 1;
-	loadImages();
+	if (num == 5)
+	{
+		endGame();
+	}
+	else
+	{
+		num += 1;
+		loadImages();
+		result.style.visibility = "hidden";
+	}
 }
+
+
+
+
